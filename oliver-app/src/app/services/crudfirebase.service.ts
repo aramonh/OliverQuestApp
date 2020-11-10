02d18f6747eb6706fef1a357b6eb6d5f3e749459
aramonh <aramonh@unal.edu.co>
2020-11-10 00:10:14 -0500

    last

--- oliver-app/src/app/services/crudfirebase.service.ts
+++ oliver-app/src/app/services/crudfirebase.service.ts
@@ -1,89 +1,76 @@
-import { Injectable } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/firestore';
-import { rejects } from 'assert';
-import { resolve } from 'dns';
-import { send } from 'process';
-import { error } from 'protractor';
-import { Quest } from '../interfaces/interfaces';
-import { QuestPagesPage } from '../pages/quest-pages/quest-pages.page';
+import { Injectable } from "@angular/core";
+import { AngularFirestore } from "@angular/fire/firestore";
+import { rejects } from "assert";
+import { resolve } from "dns";
+import { send } from "process";
+import { error } from "protractor";
+import { Quest } from "../interfaces/interfaces";
+import { QuestPagesPage } from "../pages/quest-pages/quest-pages.page";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class CRUDfirebaseService {
-data:any
-  constructor(private firestore: AngularFirestore) { }
+  data: any;
+  constructor(private firestore: AngularFirestore) {}
 
+  async getDataAll(collection: string) {
+    try {
+      var res = [];
+      await this.firestore.firestore
+        .collection(collection)
+        .onSnapshot((querySnap) => {
+          querySnap.forEach((doc) => {
+            console.log("Data", doc.data());
+            res.push(doc.data());
+          });
+        });
+      console.log("REs", res);
+      return res;
+    } catch (error) {}
+  }
 
-
-
-  
-
-async getDataAll(collection:string){
-try {
-  var res=[];
-     await this.firestore.firestore.collection(collection)
-     .onSnapshot(querySnap => {
-
-      
-        querySnap.forEach(doc => {
-          console.log("Data", doc.data())
-          res.push( doc.data());
-        })
-
-
-        
-        
-        
-     })
-     console.log("REs", res)
-     return res;
-
-   
-} catch (error) {
-  
-}
-}
-
-
- 
-
-  getByID(colleccion:string, id:any){
-    var data:any
-      this.firestore.doc( colleccion+'/' + id ).valueChanges()
-      .subscribe( data=>{
-      
-        this.data= data;
-      }
-    );
-    data=this.data;
-    this.data="";
-    console.log("DATA",data)
+  getByID(colleccion: string, id: any) {
+    var data: any;
+    this.firestore
+      .doc(colleccion + "/" + id)
+      .valueChanges()
+      .subscribe((data) => {
+        this.data = data;
+      });
+    data = this.data;
+    this.data = "";
+    console.log("DATA", data);
     return data;
   }
 
-
-  async deleteData(colleccion:string, id:any){
-     await this.firestore.collection(colleccion).doc(id).delete();
+  async deleteData(colleccion: string, id: any) {
+    await this.firestore.collection(colleccion).doc(id).delete();
   }
 
-  async createData(colleccion:string, data:any ){
-    await this.firestore.collection(colleccion).add(data)
-          .then( data => {
-            console.log('created', data);
-            return data;
-          }).catch(err=>{
-            console.log(err)
-          });;
+  async createData(colleccion: string, data: any) {
+    await this.firestore
+      .collection(colleccion)
+      .add(data)
+      .then((data) => {
+        console.log("created", data);
+        return data;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
- async updateData(colleccion:string,id:any, data: any ){
-    await this.firestore.collection(colleccion).doc(id).update(data)
-    .then( data => {
-      console.log('updated', data);
-    }).catch(err=>{
-      console.log(err)
-    });
-
+  async updateData(colleccion: string, id: any, data: any) {
+    await this.firestore
+      .collection(colleccion)
+      .doc(id)
+      .update(data)
+      .then((data) => {
+        console.log("updated", data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 }
